4d06d7b12da2b4218efcfb02c084811e16bc1ef0
GHouste <imghouste@gmail.com>
2026-01-29 16:57:46 +0100

    make the game fucking work

--- blackjack.py
+++ blackjack.py
@@ -14,16 +14,11 @@ class player:
         bet_placed = False
         while bet_placed == False:
             user_input = input("how much money you want to bet: ")
-            print(user_input)
-            print(type(user_input))
             bet_money = int(user_input)
-            print(f"bet money: {bet_money}")
-            print(type(bet_money))
-            
-            if type(bet_money)  != int:
-                print("Debil")
+            print(f"money betted: {bet_money}")
             if bet_money > self.balance:
                 print(f"You dont have enough money")
+
             else:
                 bet_placed = True
                 self.balance = self.balance - bet_money
@@ -62,19 +57,59 @@ def game_cycle(player, ai):
     player_doubledown = False
     player_insured = False
     
-    if player.points < 21 or ai.points < 21:
-        print(turn)
+    if player.points < 21 or ai.points < 21 or player_doubledown != True and len(player.hand) != 3:
         print("-------Black Jack-------\n")
         print(f"Your side:")
         print(f"- Balance: {player.balance}")
+        print(f"- Betted: {player.bet}")
         print(f"- Hand: {player.hand}")
         print(f"- Points: {player.points}\n")
         print(f"Dealer`s side:")
         print(f"- Hand: {ai.hand}")
         print(f"- Points: {ai.points}\n")
+       
+        if len(player.hand) == 1 and "ace" in ai.hand:
+            print(f"- - - - - - - - - - - - - - -")
+            print(f"(1) Hit")
+            print(f"(2) pass")
+            print(f"(3) insurance")
+            player_choice = input("your decision: ")
+                
+            match player_choice:
+                case "1":
+                    player.draw_card()
+                    ai.draw_card()
+                            
+                case "2":
+                    ai.draw_card()
+                case "3":
+                    player_insured = True
+                                
+                case _:
+                    print("error try normal option")
 
-        if turn == 2:
-            print(turn)
+        if len(player.hand) == 2 and player_insured == False:
+            print(f"- - - - - - - - - - - - - - -")
+            print(f"(1) Hit")
+            print(f"(2) pass")
+            print(f"(3) double down")
+            player_choice = input("your decision: ")
+                
+            match player_choice:
+                case "1":
+                    player.draw_card()
+                    ai.draw_card()
+                            
+                case "2":
+                    ai.draw_card()
+                case "3":
+                    player_doubledown = True
+                    player.draw_card()
+                    ai.draw_card()
+                                
+                case _:
+                    print("error try normal option")
+
         else:
                 print(f"- - - - - - - - - - - - - - -")
                 print(f"(1) Hit")
@@ -91,29 +126,69 @@ def game_cycle(player, ai):
                                 
                     case _:
                         print("error try normal option")
-                                
-    if player.points >= 21:
-        print(f"you lost {player.bet} ")
+    
+    if player_doubledown ==True and len(player.hand) == 3:
+        if player.points < 21 and player.points > ai.points:
+            player.betted = False
+            player.points = 0
+            ai.points = 0
+            player.hand.clear()
+            ai.hand.clear()
+            played_cards.clear()
+            player.add_money(4 * player.bet)
+            print(f"you won {4 * player.bet}$")
+        else:
+            print(f"you lost {2 * player.bet} ")
+            player.betted = False
+            player.points = 0
+            ai.points = 0
+            player.hand.clear()
+            ai.hand.clear()
+            played_cards.clear()                                
+
+    if player.points >= 21 and player_insured == False:
+        print(f"you lost {player.bet}$")
         player.betted = False
         player.points = 0
+        ai.points = 0
+        player.hand.clear()
+        ai.hand.clear()
+        played_cards.clear()
+
+    if player.points >= 21 and player_insured == True:
+        print(f"you insurred and lost 0$")
+        player.betted = False
+        player.points = 0
+        ai.points = 0
+        player.add_money(player.bet)
+        player.hand.clear()
+        ai.hand.clear()
+        played_cards.clear()
+
+
     elif ai.points >=21:
-        pass
+        player.betted = False
+        player.points = 0
+        ai.points = 0
+        player.hand.clear()
+        ai.hand.clear()
+        played_cards.clear()
+        player.add_money(2 * player.bet)
+        print(f"you won {2 * player.bet}$")
     
 def main():
-    player_1 = player(100, 20)
+    player_1 = player(100, 0)
     dealer = player(0, 0)
     global played_cards 
     played_cards = []
     global decks 
     decks = 1
-    global turn
-    turn = 1
     decks_number_choosen = False
     game_on = True
     global betted
     betted = False
     
-    """
+    
     print("-------Black Jack-------\n")
     while decks_number_choosen == False:
         user_input = input("how many decks you want to use(1-8): ")
@@ -122,25 +197,17 @@ def main():
             print("decks number should be between 1 and 8")
         else:
             decks_number_choosen = True
-    """
+   
     while game_on == True:
         if player_1.betted == False:
             player_1.betting()
             player_1.betted = True
+        elif player_1.balance == 0:
+            game_on = False
+            print("You lost everything")
         else:    
             game_cycle(player_1, dealer)
-            turn = turn + 1
     
 main()
 
 
-"""
-to do list:
-    główna pętla gry
-    pozostałe formy obstawiania
-    dodaj podwojenie zakładu
-    dodaj ubezpieczenie
-    zrób coś jak już się karta wylosowała odpowiednio
-    
-
-"""
